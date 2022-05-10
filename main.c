025aa700f60655477f08a34cd99e11086b3e1a5d
ismetcanbyk <ismetcanbyk@gmail.com>
2022-05-10 14:51:38 +0300

    main

--- main.c
+++ main.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 
 void sezar(size_t x, char *a);
+int sifreBul(int b);
 
 struct ogrenci{
     char name[20];
     char surname[12];
     int ogrNum;
+    int sifre;
 }ogr;
 
 void main()
 {
-
+    int giriSifre;
     FILE *enc;
 
-    enc = fopen("sifreleme.txt","w+");
+    enc = fopen("sifrelimetin.txt","w+");
     if(enc==NULL){
         printf("Dosya Acilamadi!");
     }else{
@@ -40,7 +42,23 @@ void main()
     fprintf(enc,ogr.name);
     fprintf(enc,ogr.surname);
     fclose(enc);
+    printf("\n");
+    printf("Ogrenci Numarasi Gir:");
+    scanf("%d",&ogr.ogrNum);
+    printf("\n");
+    TRY:
+    printf("Sifre Gir:");
+    scanf("%d",&giriSifre);
+    printf("\n");
+
+    ogr.sifre = sifreBul(ogr.ogrNum);
+
+    if(ogr.sifre==giriSifre){
+        printf("giris basarili");
+    }else{printf("Hatali Parola"); printf("\n"); goto TRY;}
     }
+    printf("\n");
+    printf("sifre:%d",ogr.sifre);
 
 
 }
@@ -59,5 +77,25 @@ void sezar(size_t x, char *a)
     }
 }
 
+int sifreBul(int b){
+    int key=0;
+    int x, y;
+    for(int i=0;i<=9;i++){
+
+
+            x = (b % 10);
+        b= b/10;
+        if(x==7){y=7;}
+        else if(x==5){y=5;}
+        else if(x==3){y=3;}
+        else if(x==2){y=2;}
+        else if(y>key){
+        key=y;
+         }
+
+
+    }
+    return key;
+}
 
 
